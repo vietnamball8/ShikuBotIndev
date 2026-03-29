8cef610f299eccf1a72ae320c9042e23df4b9ef7
vietnamball8 <caominhkhang190823@gmail.com>
2026-03-29 19:46:44 +0700

    Modify webserver to read port from environment variable
    Update webserver to use environment variable for port.

--- webserver.py
+++ webserver.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from threading import Thread
 
@@ -7,7 +8,8 @@ def home():
     return "ShikuBot online!"
 
 def run():
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
     
 def keep_alive():
     t = Thread(target=run)
